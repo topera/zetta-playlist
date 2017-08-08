478b7c52cb6c4497f5efd611466f79d1ae6941b3
topera <topera@gmail.com>
2017-08-07 22:24:32 -0300

    adding version api call + removing author info

---
+++ src/main/java/com/topera/zp/model/zp/Version.java
@@ -0,0 +1,15 @@
+package com.topera.zp.model.zp;
+
+public class Version {
+
+    private String name;
+
+    public Version(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
